73845bbee8fa7f3de4602af670f7a3c49326631b
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-09-10 15:24:24 -0700

    feat: add like functionality to photo item

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
   const { imageSource, profile, username, location } = props.data;
 
   return (
     <div className="photo-list__item">
+      <PhotoFavButton></PhotoFavButton>
       <img className="photo-list__image" src={imageSource} alt="Image"></img>
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={profile} alt={`Profile Image for ${username}`}></img>
