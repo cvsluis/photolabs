3f2d96c8937fb67498ba6f78a1f72b80e4646f4a
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-09-10 14:21:29 -0700

    add: update styling for photo list item component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,12 +3,20 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
+  const { imageSource, profile, username, location } = props.data;
+
   return (
-    <div>
-      <img src={props.data.imageSource}></img>
-      <img src={props.data.profile}></img>
-      <p>{props.data.username}</p>
-      <p>{props.data.location.city}, {props.data.location.country}</p>
+    <div className="photo-list__item">
+      <img className="photo-list__image" src={imageSource} alt="Image"></img>
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={profile} alt={`Profile Image for ${username}`}></img>
+        <div className="photo-list__user-info">
+          <span>{username}</span>
+          <div className="photo-list__user-location">
+            {location.city}, {location.country}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
