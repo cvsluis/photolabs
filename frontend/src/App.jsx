b4ada622e2de0c4dd388c53f9365516cf67e8ace
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-09-10 16:38:35 -0700

    feat: implement top nav bar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-
-import './App.scss';
-import TopicList from 'components/TopicList';
+import TopNavigationBar from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 
-// Note: Rendering a single component to build components in isolation
+import './App.scss';
+
 const App = () => {
   return (
     <div className="App">
-      <TopicList />
-      {/* <PhotoList /> */}
+      <TopNavigationBar />
+      <PhotoList />
     </div>
   );
 };
