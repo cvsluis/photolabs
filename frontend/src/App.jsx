850d8b11e29a8e5942396c188cae74d4af3f3ea6
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-09-10 16:40:48 -0700

    feat: add home route component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import TopNavigationBar from 'components/TopNavigationBar';
-import PhotoList from 'components/PhotoList';
+
 
 import './App.scss';
+import HomeRoute from 'routes/HomeRoute';
 
 const App = () => {
   return (
     <div className="App">
-      <TopNavigationBar />
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
