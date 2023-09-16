7e8a1509de643ef50b3fc5dc071b452f5101339f
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-09-16 15:11:03 -0700

    feat: show modal when user clicks on photo

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -4,10 +4,10 @@ import PhotoListItem from "./PhotoListItem";
 
 import "../styles/PhotoList.scss";
 
-const PhotoList = ({ isLiked, toggleLike, photos }) => {
+const PhotoList = ({ isLiked, toggleLike, photos, showModal }) => {
   const photoList = photos.map(photo => {
     return (
-      <PhotoListItem key={photo.id} isLiked={isLiked} toggleLike={toggleLike} photoId={photo.id} data={photo} />
+      <PhotoListItem key={photo.id} isLiked={isLiked} toggleLike={toggleLike} photoId={photo.id} data={photo} showModal={showModal}/>
     );
   });
 
