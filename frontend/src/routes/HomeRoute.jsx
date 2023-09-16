742d0455750f683164e968cf901ba96a32b6e9f3
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-09-16 14:46:40 -0700

    feat: display notification for favourited photos

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -9,10 +9,11 @@ const HomeRoute = () => {
   const [like, setLike] = useState([]);
   const toggleLike = (photoId) => like.includes(photoId) ? setLike(like.filter(e => e !== photoId)) : setLike([...like, photoId]);
   const isLiked = (photoId) => like.includes(photoId);
+  const isFavPhotoExist = like.length > 0;
 
   return (
     <div className="home-route">
-      <TopNavigationBar />
+      <TopNavigationBar isFavPhotoExist={isFavPhotoExist}/>
       <PhotoList isLiked={isLiked} toggleLike={toggleLike} photos={photos} />
     </div>
   );
