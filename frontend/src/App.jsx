1cc815170f78818a7eff975403c523695e0c7424
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-09-17 16:18:42 -0700

    fix: refactor side peek render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 const App = () => {
+  // state for favouriting photos
+  const [like, setLike] = useState([]);
+  const toggleLike = (photoId) => like.includes(photoId) ? setLike(like.filter(e => e !== photoId)) : setLike([...like, photoId]);
+  const isLiked = (photoId) => like.includes(photoId);
+  const isFavPhotoExist = like.length > 0;
+
+  // state for keeping track of selected photo when opening modal
+  const [selectedPhoto, setSelectedPhoto] = useState();
+
+  // state for opening the photo details modal
+  const [modal, setModal] = useState(false);
+  const showModal = (photo) => {
+    setModal(true);
+    setSelectedPhoto(photo);
+  };
+  const hideModal = () => setModal(false);
+
   return (
     <div className="App">
-      <HomeRoute />
+      <HomeRoute isFavPhotoExist={isFavPhotoExist} isLiked={isLiked} toggleLike={toggleLike} showModal={showModal} />
+      {modal && <PhotoDetailsModal hideModal={hideModal} selectedPhoto={selectedPhoto} isLiked={isLiked} toggleLike={toggleLike} />}
     </div>
   );
 };
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,34 +1,15 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import TopNavigationBar from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 import photos from "mocks/photos";
-import PhotoDetailsModal from './PhotoDetailsModal';
 
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = () => {
-  // state for favouriting photos
-  const [like, setLike] = useState([]);
-  const toggleLike = (photoId) => like.includes(photoId) ? setLike(like.filter(e => e !== photoId)) : setLike([...like, photoId]);
-  const isLiked = (photoId) => like.includes(photoId);
-  const isFavPhotoExist = like.length > 0;
-
-  // state for keeping track of selected photo when opening modal
-  const [selectedPhoto, setSelectedPhoto] = useState();
-
-  // state for opening the photo details modal
-  const [modal, setModal] = useState(false);
-  const showModal = (photo) => {
-    setModal(true);
-    setSelectedPhoto(photo);
-  };
-  const hideModal = () => setModal(false);
-
+const HomeRoute = ({ isFavPhotoExist, isLiked, toggleLike, showModal }) => {
   return (
     <div className="home-route">
       <TopNavigationBar isFavPhotoExist={isFavPhotoExist}/>
       <PhotoList isLiked={isLiked} toggleLike={toggleLike} photos={photos} showModal={showModal}/>
-      {modal && <PhotoDetailsModal hideModal={hideModal} selectedPhoto={selectedPhoto} isLiked={isLiked} toggleLike={toggleLike}/>}
     </div>
   );
 };
