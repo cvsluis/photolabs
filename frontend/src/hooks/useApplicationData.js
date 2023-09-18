57146bed8eb4cb43c67e1e111504060e8ff06b33
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-09-17 23:25:28 -0700

    fix: remove comments

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -26,9 +26,3 @@ export const useApplicationData = () => {
     onClosePhotoDetailsModal
   };
 };
-
-
-// The state object will contain the entire state of the application.
-// The updateToFavPhotoIds action can be used to set the favourite photos.
-// The setPhotoSelected action can be used when the user selects a photo.
-// The onClosePhotoDetailsModal action can be used to close the modal.
