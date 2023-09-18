95da2a872bbde73e46b325c316e3510bc8771412
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-09-17 23:19:34 -0700

    fix: refactor state custom hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,19 @@ import { useApplicationData } from 'hooks/useApplicationData';
 
 const App = () => {
   const {
-    toggleLike,
-    isLiked,
-    isFavPhotoExist,
-    selectedPhoto,
-    modal,
-    showModal,
-    hideModal
+    state: { likes, selectedPhoto, modal },
+    updateToFavPhotoIds,
+    setPhotoSelected,
+    onClosePhotoDetailsModal
   } = useApplicationData();
+
+  const isLiked = (photoId) => likes.includes(photoId);
+  const isFavPhotoExist = likes.length > 0;
 
   return (
     <div className="App">
-      <HomeRoute isFavPhotoExist={isFavPhotoExist} isLiked={isLiked} toggleLike={toggleLike} showModal={showModal} />
-      {modal && <PhotoDetailsModal showModal={showModal} hideModal={hideModal} selectedPhoto={selectedPhoto} isLiked={isLiked} toggleLike={toggleLike} />}
+      <HomeRoute isFavPhotoExist={isFavPhotoExist} isLiked={isLiked} toggleLike={updateToFavPhotoIds} showModal={setPhotoSelected} />
+      {modal && <PhotoDetailsModal showModal={setPhotoSelected} hideModal={onClosePhotoDetailsModal} selectedPhoto={selectedPhoto} isLiked={isLiked} toggleLike={updateToFavPhotoIds} />}
     </div>
   );
 };
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,30 +1,34 @@
 import { useState } from 'react';
 
 export const useApplicationData = () => {
-  // state for favouriting photos
   const [likes, setLikes] = useState([]);
-  const toggleLike = (photoId) => likes.includes(photoId) ? setLikes(likes.filter(e => e !== photoId)) : setLikes([...likes, photoId]);
-  const isLiked = (photoId) => likes.includes(photoId);
-  const isFavPhotoExist = likes.length > 0;
-
-  // state for keeping track of selected photo when opening modal
-  const [selectedPhoto, setSelectedPhoto] = useState();
-
-  // state for opening the photo details modal
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
   const [modal, setModal] = useState(false);
-  const showModal = (photo) => {
+
+  const updateToFavPhotoIds = (photoId) => {
+    likes.includes(photoId) ? setLikes(likes.filter(e => e !== photoId)) : setLikes([...likes, photoId]);
+  };
+
+  const setPhotoSelected = (photo) => {
     setModal(true);
     setSelectedPhoto(photo);
   };
-  const hideModal = () => setModal(false);
+
+  const onClosePhotoDetailsModal = () => {
+    setModal(false);
+    setSelectedPhoto(null);
+  };
 
   return {
-    toggleLike,
-    isLiked,
-    isFavPhotoExist,
-    selectedPhoto,
-    modal,
-    showModal,
-    hideModal
+    state: { likes, selectedPhoto, modal },
+    updateToFavPhotoIds,
+    setPhotoSelected,
+    onClosePhotoDetailsModal
   };
 };
+
+
+// The state object will contain the entire state of the application.
+// The updateToFavPhotoIds action can be used to set the favourite photos.
+// The setPhotoSelected action can be used when the user selects a photo.
+// The onClosePhotoDetailsModal action can be used to close the modal.
