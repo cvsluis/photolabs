961051a8912ae8ef16ed3c754271f3b7e8a135f8
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-09-18 21:09:30 -0700

    feat: implement useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,26 +1,51 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+const INITIAL_STATE = {
+  likes: [],
+  selectedPhoto: null,
+  modal: false
+};
+
+const ACTIONS = {
+  TOGGLE_LIKE: 'TOGGLE_LIKE',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  CLOSE_PHOTO: 'CLOSE_PHOTO'
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+
+    case ACTIONS.TOGGLE_LIKE:
+      const photoId = action.payload;
+      if (state.likes.includes(photoId)) {
+        return { ...state, likes: state.likes.filter(id => id !== photoId) };
+      };
+      return { ...state, likes: [...state.likes, photoId] };
+
+    case ACTIONS.SELECT_PHOTO:
+      return { ...state, modal: true, selectedPhoto: action.payload };
+
+    case ACTIONS.CLOSE_PHOTO:
+      return { ...state, modal: false, selectedPhoto: null };
+
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+};
 
 export const useApplicationData = () => {
-  const [likes, setLikes] = useState([]);
-  const [selectedPhoto, setSelectedPhoto] = useState(null);
-  const [modal, setModal] = useState(false);
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
 
-  const updateToFavPhotoIds = (photoId) => {
-    likes.includes(photoId) ? setLikes(likes.filter(e => e !== photoId)) : setLikes([...likes, photoId]);
-  };
+  const updateToFavPhotoIds = (photoId) => dispatch({ type: ACTIONS.TOGGLE_LIKE, payload: photoId });
 
-  const setPhotoSelected = (photo) => {
-    setModal(true);
-    setSelectedPhoto(photo);
-  };
+  const setPhotoSelected = (photo) => dispatch({ type: ACTIONS.SELECT_PHOTO, payload: photo });
 
-  const onClosePhotoDetailsModal = () => {
-    setModal(false);
-    setSelectedPhoto(null);
-  };
+  const onClosePhotoDetailsModal = () => dispatch({ type: ACTIONS.CLOSE_PHOTO });
 
   return {
-    state: { likes, selectedPhoto, modal },
+    state,
     updateToFavPhotoIds,
     setPhotoSelected,
     onClosePhotoDetailsModal
